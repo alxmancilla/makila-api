14e1295c84898a24f0e15c7a68ad27c81153027c
Alejandro Mancilla <2440015+alxmancilla@users.noreply.github.com>
2024-11-12 18:57:14 -0600

    order service

---
+++ api/src/main/java/org/makila/api/controller/postgresql/OrderController.java
@@ -0,0 +1,32 @@
+package org.makila.api.controller.postgresql;
+
+import org.makila.api.model.postgresql.Order;
+import org.makila.api.service.postgresql.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.time.LocalDateTime;
+
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return ResponseEntity.ok(orderService.getAllOrders());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Integer id) {
+        return ResponseEntity.ok(orderService.getOrderById(id));
+    }
+    
+    @GetMapping("/dates/{minDate}-{maxDate}")
+    public ResponseEntity<List<Order>> getOrdersBetweeOrderDates(@RequestParam LocalDateTime minDate,@RequestParam LocalDateTime maxDate ) {
+        return ResponseEntity.ok(orderService.getOrdersBetweenOrderDates(minDate, maxDate));
+    }
+}
