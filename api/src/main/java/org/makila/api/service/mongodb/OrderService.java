95b3c991467f7c99e96516cd93353638f1dd7a3c
Alejandro Mancilla <2440015+alxmancilla@users.noreply.github.com>
2024-11-14 09:29:52 -0600

    load testing examples

--- api/src/main/java/org/makila/api/service/mongodb/OrderService.java
+++ api/src/main/java/org/makila/api/service/mongodb/OrderService.java
@@ -1,5 +1,17 @@
 package org.makila.api.service.mongodb;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 public class OrderService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public int getMaxOrderID() {
+        Query query = entityManager.createQuery("SELECT COUNT(u) FROM User u");
+        Long count = (Long) query.getSingleResult();
+        return count.intValue();
+    }
+
     
 }
